1be0c5fc36624d7ffd5513360c13efa23f27263f
Goowen <Gooweningame@gmail.com>
2023-03-08 14:50:36 +0100

    Added Trashcontainer

--- src/main/java/me/goowen/projectm/modules/misc/MiscModule.java
+++ src/main/java/me/goowen/projectm/modules/misc/MiscModule.java
@@ -1,12 +1,17 @@
 package me.goowen.projectm.modules.misc;
 
 import me.goowen.projectm.ProjectM;
+import me.goowen.projectm.modules.misc.listeners.PlayerContainerInteractListener;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class MiscModule {
     private final ProjectM projectM = ProjectM.getInstance();
 
     public MiscModule() {
+
+        Bukkit.getPluginManager().registerEvents(new PlayerContainerInteractListener(), projectM);
+
         projectM.getLog().info(ChatColor.DARK_AQUA + "[MiscModule] De module is succesvol geladen!");
     }
 }
