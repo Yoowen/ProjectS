61aa5c017affd8332da09105a498f4ffebfa7690
Goowen <Gooweningame@gmail.com>
2023-11-04 13:42:14 +0100

    Added Shiv

--- src/main/java/me/goowen/projectm/modules/misc/listeners/PlayerDoorInteractListener.java
+++ src/main/java/me/goowen/projectm/modules/misc/listeners/PlayerDoorInteractListener.java
@@ -1,7 +1,9 @@
 package me.goowen.projectm.modules.misc.listeners;
 
 import me.goowen.projectm.ProjectM;
+import me.goowen.projectm.modules.misc.MiscModule;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.data.type.Door;
@@ -12,6 +14,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 public class PlayerDoorInteractListener implements Listener {
     private final ProjectM projectM = ProjectM.getInstance();
@@ -22,21 +26,39 @@ public class PlayerDoorInteractListener implements Listener {
         if (event.getAction() == Action.PHYSICAL) return;
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
+        Player player = event.getPlayer();
+        if (player.getScoreboardTags().contains("stopShivUse")) return;
+
         ItemStack itemStack = event.getItem();
         if (itemStack == null) return;
         if (itemStack.getItemMeta() == null) return;
-        if (!itemStack.getItemMeta().getDisplayName().equalsIgnoreCase("Shiv")) return;
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (!itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.WHITE + "Shiv")) return;
         if (event.getClickedBlock() == null) return;
-        if (!event.getClickedBlock().getType().equals(Material.WARPED_DOOR)) return;
+        if (!event.getClickedBlock().getType().equals(Material.IRON_DOOR)) return;
 
-        Door door = (Door) event.getClickedBlock();
-        Player player = event.getPlayer();
+        Door door = (Door) event.getClickedBlock().getBlockData();
 
         door.setOpen(true);
+        event.getClickedBlock().setBlockData(door);
+
         player.playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 1f, 1f);
+
+        MiscModule miscModule = ProjectM.getMiscModule();
+        Integer currentDurability = itemMeta.getPersistentDataContainer().get(miscModule.getShivDurabilityNamespacedKey(), PersistentDataType.INTEGER) - 1;
+        if (currentDurability < 1) {
+            player.getInventory().setItem(event.getHand(), null);
+            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK,1,1);
+        } else {
+            player.getInventory().setItem(event.getHand(), miscModule.getWrench(currentDurability));
+        }
+        player.updateInventory();
+        player.addScoreboardTag("stopShivUse");
 
         Bukkit.getScheduler().runTaskLater(projectM , () -> {
             door.setOpen(false);
+            event.getClickedBlock().setBlockData(door);
+            player.removeScoreboardTag("stopShivUse");
         }, 20 * 30);
     }
 }
